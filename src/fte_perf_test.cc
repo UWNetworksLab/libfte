54da67c2070016d3b44946d8401c0a28cfc138cb
Kevin P. Dyer <kpdyer@gmail.com>
2014-07-06 22:16:35 -0700

    Issue #18: fix the fte performance tet

--- src/fte_perf_test.cc
+++ src/fte_perf_test.cc
@@ -15,17 +15,17 @@ void FteExample() {
   std::string K("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"); // 128 bits, in hex
   fte::Fte fteObj = fte::Fte();
   fteObj.set_key(K);
-  std::string regex = "^.+$";
+  std::string regex = "^.*$";
   std::string dfa;
   regex2dfa::Regex2Dfa(regex, &dfa);
   fteObj.SetLanguages(dfa, 1024,
                       dfa, 1024);
-  std::string input_plaintext = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+  std::string input_plaintext = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
   std::string ciphertext, output_plaintext;
 
   double duration;
   std::clock_t start = std::clock();
-  for (uint32_t i = 0; i < 1024; ++i) {
+  for (uint32_t i = 0; i < 1000; ++i) {
     fteObj.Encrypt(input_plaintext, &ciphertext);
   }
   duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
